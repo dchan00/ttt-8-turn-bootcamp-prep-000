f57bcb0997649fcb39f9bb9ca80b296484ed16c4
DANIEL CHAN <dchan513@msn.com>
2017-09-29 02:34:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if(index.between(0,8) && !position_taken?(board,index))
+  if(index.between?(0,8) && !position_taken?(board,index))
     true
   else
     false
